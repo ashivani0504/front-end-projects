6e3979b3c09fd8dbec43aa827ba69fc41aae5242
Shivani <ashivani0504@gmail.com>
2024-07-03 06:14:25 +0530

    adding starRating

--- src/Components/StarRating.jsx
+++ src/Components/StarRating.jsx
@@ -1,6 +1,12 @@
-import styles from ""
+import styles from "../Styles/StarRating.module.css";
+
 function StarRating() {
-  return <div></div>;
+  return (
+    <div className={styles.starRating}>
+      <h1> Star Rating </h1>
+      <div></div>
+    </div>
+  );
 }
 
 export default StarRating;
